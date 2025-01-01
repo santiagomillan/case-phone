b5628af2a0102889760247becd2d108e98892dc2
Santiago Millan <santiagomillanpardo@gmail.com>
2024-12-31 20:35:47 -0500

    Auth: kinde. P1

--- src/componets/Navbar.tsx
+++ src/componets/Navbar.tsx
@@ -1,31 +1,43 @@
 import Link from "next/link";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import { buttonVariants } from "@/components/ui/button";
-import { ArrowRight } from "lucide-react";
 
-const Navbar = () => {
-  const user = undefined;
-  const isAdmin = false;
+import { ArrowRight } from "lucide-react";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
+const Navbar = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  const isAdmin = user?.email === process.env.ADMIN_EMAIL;
+
   return (
-    <nav className="sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all ">
+    <nav className="sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
       <MaxWidthWrapper>
         <div className="flex h-14 items-center justify-between border-b border-zinc-200">
           <Link href="/" className="flex z-40 font-semibold">
-            Case<span className="text-green-600">Cobra</span>
+            case<span className="text-green-600">cobra</span>
           </Link>
+
           <div className="h-full flex items-center space-x-4">
             {user ? (
               <>
                 <Link
                   href="/api/auth/logout"
-                  className={buttonVariants({ size: "sm", variant: "ghost" })}
+                  className={buttonVariants({
+                    size: "sm",
+                    variant: "ghost",
+                  })}
                 >
                   Sign out
                 </Link>
                 {isAdmin ? (
                   <Link
-                    href="/api/auth/logout"
-                    className={buttonVariants({ size: "sm", variant: "ghost" })}
+                    href="/dashboard"
+                    className={buttonVariants({
+                      size: "sm",
+                      variant: "ghost",
+                    })}
                   >
                     Dashboard ✨
                   </Link>
@@ -45,10 +57,14 @@ const Navbar = () => {
               <>
                 <Link
                   href="/api/auth/register"
-                  className={buttonVariants({ size: "sm", variant: "ghost" })}
+                  className={buttonVariants({
+                    size: "sm",
+                    variant: "ghost",
+                  })}
                 >
                   Sign up
                 </Link>
+
                 <Link
                   href="/api/auth/login"
                   className={buttonVariants({
@@ -58,7 +74,9 @@ const Navbar = () => {
                 >
                   Login
                 </Link>
+
                 <div className="h-8 w-px bg-zinc-200 hidden sm:block" />
+
                 <Link
                   href="/configure/upload"
                   className={buttonVariants({
