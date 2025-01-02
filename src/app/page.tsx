a0942c95d0c8e2de11857e8f038e19918f5105a3
Santiago Millan <santiagomillanpardo@gmail.com>
2025-01-01 19:20:43 -0500

    Icons underline. 1:11

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import MaxWidthWrapper from "@/componets/MaxWidthWrapper";
 import Phone from "@/componets/Phone";
 import { Check, Star } from "lucide-react";
+import { Icons } from "@/componets/icons";
 // import Image from "next/image";
 
 export default function Home() {
@@ -94,7 +95,12 @@ export default function Home() {
         <MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-32">
           <div className="flex flex-col lg:flex-row items-center gap-4 sm:gap-6 ">
             <h2 className="order-1 mt-2 -tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
-              What our customers say
+              What our{" "}
+              <span className="relative px-2 ">
+                customers{" "}
+                <Icons.underline className="hidden sm:block pointer-events-none absolute inset-x-0 -bottom-6 text-green-500" />
+              </span>{" "}
+              say
             </h2>
           </div>
         </MaxWidthWrapper>
