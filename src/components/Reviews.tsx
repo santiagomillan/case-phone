c7a6f28c8eac5b91c7ef1479c7d2503deee3f2ec
Santiago Millan <santiagomillanpardo@gmail.com>
2025-01-03 23:31:40 -0500

    Reviews. framer-motion, animation P2

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -1,5 +1,16 @@
-// "use client";
+"use client";
+import { useRef } from "react";
 import MaxWidthWrapper from "./MaxWidthWrapper";
+
+function ReviewGrid() {
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  return (
+    <div
+      ref={containerRef}
+      className="bg-red-500 relative -mx-4 mt-16 grid h-[49rem] max-h-[150vh] grid-cols-1 items-start gap-8 overflow-hidden px-4 sm:mt20 md:grid-cols-2 lg:grid-cols-3"
+    ></div>
+  );
+}
 
 export function Reviews() {
   return (
@@ -7,8 +18,9 @@ export function Reviews() {
       <img
         aria-hidden="true"
         src="/what-people-are-buying.png"
-        className="absolute select-none hidden xl:block -left-32 top-1/3"
+        className=" absolute select-none hidden xl:block -left-32 top-1/3"
       />
+      <ReviewGrid />
     </MaxWidthWrapper>
   );
 }
