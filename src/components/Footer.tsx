dca43b750cd7073b12ee2f2650f1f409e2b7ca30
Santiago Millan <santiagomillanpardo@gmail.com>
2025-01-07 00:05:14 -0500

    Footer P1 2:12

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import MaxWidthWrapper from "./MaxWidthWrapper";
+
+const Footer = () => {
+  return (
+    <footer className="bg-white h-20 relative">
+      <MaxWidthWrapper>
+        <div></div>
+      </MaxWidthWrapper>
+    </footer>
+  );
+};
+export default Footer;
