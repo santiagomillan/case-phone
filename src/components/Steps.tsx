36bbfedc6867448409a46c1a83a3959afa27e712
Santiago Millan <santiagomillanpardo@gmail.com>
2025-01-19 00:42:26 -0500

    Steps P3 Finish the steps list. 2:24

--- src/components/Steps.tsx
+++ src/components/Steps.tsx
@@ -74,6 +74,23 @@ const Steps = () => {
                   </span>
                 </span>
               </span>
+              {/* separator */}
+              {i !== 0 ? (
+                <div className="absolute inset-0 hidden w-3 lg:block">
+                  <svg
+                    className="h-full w-full text-gray-300"
+                    viewBox="0 0 12 82"
+                    fill="none"
+                    preserveAspectRatio="none"
+                  >
+                    <path
+                      d="M0.5 0V31L10.5 41L0.5 51V82"
+                      stroke="currentcolor"
+                      vectorEffect="non-scaling-stroke"
+                    />
+                  </svg>
+                </div>
+              ) : null}
             </div>
           </li>
         );
