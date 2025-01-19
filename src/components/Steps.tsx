94006408cc8a2382485169565963ec202f7dbc5a
Santiago Millan <santiagomillanpardo@gmail.com>
2025-01-19 00:22:27 -0500

    Steps P2 3:20

--- src/components/Steps.tsx
+++ src/components/Steps.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { cn } from "@/lib/utils";
 import { usePathname } from "next/navigation";
 
 const STEPS = [
@@ -30,7 +31,52 @@ const Steps = () => {
         const isCompleted = STEPS.slice(i + 1).some((step) =>
           pathname.endsWith(step.url)
         );
-        return <li></li>;
+        const imgPath = `/snake-${i + 1}.png`;
+        return (
+          <li key={step.name} className="relative overflow-hidden lg:flex-1">
+            <div>
+              <span
+                className={cn(
+                  "absolute left-0 top-0 h-full w-1 bg-zinc-400 lg:bottom-0 lg:top-auto lg:h-1 lg:w-full",
+                  { "bg-zinc-700": isCurrent, "bg-primary": isCompleted }
+                )}
+                aria-hidden="true"
+              />
+              <span
+                className={cn(
+                  i !== 0 ? "lg:pl-9" : "",
+                  "flex items-center px-6 py-4 text-sm font-medium"
+                )}
+              >
+                <span className="flex-shrink-0">
+                  <img
+                    src={imgPath}
+                    className={cn(
+                      "flex h-20 w-20 object-contain items-center justify-center",
+                      {
+                        "border-none": isCompleted,
+                        "border-zinc-700": isCurrent,
+                      }
+                    )}
+                  />
+                </span>
+                <span className="ml-4 h-full mt-0.5 flex min-w-0 flex-col justify-center">
+                  <span
+                    className={cn("text-sm font-semibold text-zinc-700", {
+                      "text-primary": isCompleted,
+                      "text-zinc-700": isCurrent,
+                    })}
+                  >
+                    {step.name}
+                  </span>
+                  <span className="text-sm text-zinc-500">
+                    {step.description}
+                  </span>
+                </span>
+              </span>
+            </div>
+          </li>
+        );
       })}
     </ol>
   );
