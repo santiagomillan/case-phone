41f87279900a8ba67ef2e3b1e27dae893b242972
Santiago Millan <santiagomillanpardo@gmail.com>
2025-01-25 23:58:52 -0500

    DesignConfigurator P2 4:04

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -1,3 +1,7 @@
+"use client";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+import NextImage from "next/image";
+
 interface DesignConfiguratorProps {
   configId: string;
   imageUrl: string;
@@ -10,9 +14,22 @@ const DesignConfigurator = ({
   imageDimensions,
 }: DesignConfiguratorProps) => {
   return (
-    <div>
-      <p>{configId}</p>
-      <p>{imageUrl}</p>
+    <div className="relative mt-20 grid grid-cols-1 lg:grid-cols-3 mb-20 pb-20">
+      <div className="relative h-[37.5rem] overflow-hidden col-span-2 w-full max-w-4xl flex items-center justify-center rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2">
+        <div className="relative w-60 bg-opacity-50 pointer-events-none aspect-[896/1831]">
+          <AspectRatio
+            ratio={896 / 1831}
+            className="pointer-events-none relative z-50 aspect-[896/1831] w-full"
+          >
+            <NextImage
+              fill
+              alt="phone image"
+              src="/phone-template.png"
+              className="pointer-events-none z-50 select-none"
+            />
+          </AspectRatio>
+        </div>
+      </div>
     </div>
   );
 };
--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,3 +1,43 @@
+// import { db } from "@/db";
+// import { notFound } from "next/navigation";
+// import DesignConfigurator from "./DesignConfigurator";
+
+// interface PageProps {
+//   searchParams: {
+//     [key: string]: string | string[] | undefined;
+//   };
+// }
+
+// const Page = async ({ searchParams }: PageProps) => {
+//   const { id } = searchParams;
+//   //make db call
+//   if (!id || id !== "string") {
+//     return notFound();
+//   }
+
+//   const configuration = await db.configuration.findUnique({
+//     where: {
+//       id: id,
+//     },
+//   });
+
+//   if (!configuration) {
+//     return notFound();
+//   }
+
+//   const { imageUrl, width, height } = configuration;
+
+//   // return <p>{id}</p>;
+//   return (
+//     <DesignConfigurator
+//       configId={configuration.id}
+//       imageDimensions={{ width, height }}
+//       imageUrl={imageUrl}
+//     />
+//   );
+// };
+
+// export default Page;
 import { db } from "@/db";
 import { notFound } from "next/navigation";
 import DesignConfigurator from "./DesignConfigurator";
@@ -10,15 +50,13 @@ interface PageProps {
 
 const Page = async ({ searchParams }: PageProps) => {
   const { id } = searchParams;
-  //make db call
-  if (!id || id !== "string") {
+
+  if (!id || typeof id !== "string") {
     return notFound();
   }
 
   const configuration = await db.configuration.findUnique({
-    where: {
-      id: id,
-    },
+    where: { id },
   });
 
   if (!configuration) {
@@ -27,7 +65,6 @@ const Page = async ({ searchParams }: PageProps) => {
 
   const { imageUrl, width, height } = configuration;
 
-  // return <p>{id}</p>;
   return (
     <DesignConfigurator
       configId={configuration.id}
