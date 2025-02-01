16e19d3a7c3e42949fb8826774b7f4b8c35fd417
Santiago Millan <santiagomillanpardo@gmail.com>
2025-02-01 00:01:11 -0500

    DesignConfigurator P8 4:23

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -70,7 +70,20 @@ const DesignConfigurator = ({
 
       <div className="h-[37.5rem] flex flex-col bg-white">
         <ScrollArea className="relative flex-1 overflow-auto">
-          <div aria-hidden="true" className="absolute z-10 inset-x-0"></div>
+          <div
+            aria-hidden="true"
+            className="absolute z-10 inset-x-0 bottom-0 h-12 bg-gradient-to-t from-white pointer-events-none"
+          />
+          <div className="px-8 pb-12 pt-8">
+            <h2 className="-tracking-tight font-bold text-3xl">
+              Customize your case
+            </h2>
+            <div className="w-full h-px bg-zinc-200 my-6" />
+
+            <div className="relative mt-4 h-full flex flex-col justify-between">
+              colors
+            </div>
+          </div>
         </ScrollArea>
       </div>
     </div>
